97a2ec3d412383a686e2c7b1abcdd0c78b6b172b
Denys Petrenko <denyspetrenko91@gmail.com>
2026-01-28 22:32:30 +0200

    fix: fixed slider

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,27 +2,39 @@ import styles from "./Slider.module.scss";
 import { useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from 'embla-carousel-autoplay';
+import slide from "./slide.png"
 
 const Slider = () => {
-    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+    const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 3000 })]);
 
-    const goToPrev = () => emblaApi?.goToPrev()
-    const goToNext = () => emblaApi?.goToNext()
+    const goToPrev = () => emblaApi?.scrollPrev();
+    const goToNext = () => emblaApi?.scrollNext();
+
+    useEffect(() => {
+        if (!emblaApi) return
+        emblaApi.plugins().autoplay?.play()
+    }, [emblaApi])
 
     return (
         <div className={styles.slider}>
-            <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container">
-                    <div className="embla__slide">Slide 1</div>
-                    <div className="embla__slide">Slide 2</div>
-                    <div className="embla__slide">Slide 3</div>
+            <div className={styles["embla__viewport"]} ref={emblaRef}>
+                <div className={styles["embla__container"]}>
+                    <div className={styles["embla__slide"]}>
+                        <img src={slide} alt="slide" />
+                    </div>
+                    <div className={styles["embla__slide"]}>
+                        <img src={slide} alt="slide" />
+                    </div>
+                    <div className={styles["embla__slide"]}>
+                        <img src={slide} alt="slide" />
+                    </div>
                 </div>
             </div>
 
-            <button className="embla__prev" onClick={goToPrev}>
+            <button className={styles["embla__prev"]} onClick={goToPrev}>
                 Scroll to prev
             </button>
-            <button className="embla__next" onClick={goToNext}>
+            <button className={styles["embla__next"]} onClick={goToNext}>
                 Scroll to next
             </button>
         </div>
