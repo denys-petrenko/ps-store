c4904c21bc7c1abc7fb26113153ebdfd6af5fde1
Denys Petrenko <denyspetrenko91@gmail.com>
2026-02-03 20:04:04 +0200

    feat: add advantage section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header";
 import Slider from "./components/Slider/Slider";
+import Advantage from "./components/Advantage/Advantage";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Slider />
+      <Advantage />
     </>
   )
 }
---
+++ src/components/Advantage/Advantage.jsx
@@ -0,0 +1,11 @@
+import styles from "./Advantage.module.scss";
+
+const Advantage = () => {
+    return (
+        <section className={styles.advantage}>
+
+        </section>
+    )
+}
+
+export default Advantage;
