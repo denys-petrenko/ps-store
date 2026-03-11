de8afb3f19d2f6c1523ad6319409089d916bb581
Denys Petrenko <denyspetrenko91@gmail.com>
2026-03-11 18:19:56 +0200

    feat: add tablet styles for Favourites component

--- src/components/Main/Favourites/Favourites.jsx
+++ src/components/Main/Favourites/Favourites.jsx
@@ -1,6 +1,8 @@
 import styles from "./Favourites.module.scss";
 import ps from "../../../assets/favourites/play-station.png";
+import psDesktop from "../../../assets/favourites/ps5-desktop.png";
 import xbox from "../../../assets/favourites/xbox-x.png";
+import xboxDesktop from "../../../assets/favourites/xbox-x-desktop.png";
 import nintendo from "../../../assets/favourites/nintendo-switch.png";
 import psp from "../../../assets/favourites/ps-portable.png";
 
@@ -10,28 +12,41 @@ const Favourites = () => {
             <h2 className={styles.title}>
                 Популярні категорії нашого магазину
             </h2>
-            <div className={styles.category}>
-                <img className={styles["category-cover"]} src={ps} alt="Play Station 5" />
-                <div className={styles.shade}>
-                    <button className={styles["category-button"]}>Перейти</button>
+            <div className={styles.categories}>
+                <div className={styles.category}>
+                    <h3 className={styles["category-title"]}>play station 5</h3>
+                    <picture className={styles["category-media"]}>
+                        <source media="(max-width: 760px)" srcSet={ps} />
+                        <img className={styles["category-cover"]} src={psDesktop} alt="Play Station 5" />
+                    </picture>
+                    <div className={styles.shade}>
+                        <button className={styles["category-button"]}>Перейти</button>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.category}>
-                <img className={styles["category-cover"]} src={xbox} alt="Xbox X" />
-                <div className={styles.shade}>
-                    <button className={styles["category-button"]}>Перейти</button>
+                <div className={styles.category}>
+                    <h3 className={styles["category-title"]}>xbox series x</h3>
+                    <picture className={styles["category-media"]}>
+                        <source media="(max-width: 760px)" srcSet={xbox} />
+                        <img className={styles["category-cover"]} src={xboxDesktop} alt="Xbox X" />
+                    </picture>
+
+                    <div className={styles.shade}>
+                        <button className={styles["category-button"]}>Перейти</button>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.category}>
-                <img className={styles["category-cover"]} src={nintendo} alt="Nintendo Switch" />
-                <div className={styles.shade}>
-                    <button className={styles["category-button"]}>Перейти</button>
+                <div className={styles.category}>
+                    <h3 className={styles["category-title"]}>nintendo switch</h3>
+                    <img className={styles["category-cover"]} src={nintendo} alt="Nintendo Switch" />
+                    <div className={styles.shade}>
+                        <button className={styles["category-button"]}>Перейти</button>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.category}>
-                <img className={styles["category-cover"]} src={psp} alt="Sony Playstation Portable" />
-                <div className={styles.shade}>
-                    <button className={styles["category-button"]}>Перейти</button>
+                <div className={styles.category}>
+                    <h3 className={styles["category-title"]}>play station vr2</h3>
+                    <img className={styles["category-cover"]} src={psp} alt="Sony Playstation VR2" />
+                    <div className={styles.shade}>
+                        <button className={styles["category-button"]}>Перейти</button>
+                    </div>
                 </div>
             </div>
         </section>
