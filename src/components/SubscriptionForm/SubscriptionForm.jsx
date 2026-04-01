f987d6be2014aefaac72ec82974318c273237e5e
Denys Petrenko <denyspetrenko91@gmail.com>
2026-04-01 17:24:44 +0300

    feat: add translation for SubscriptionForm section

--- src/components/SubscriptionForm/SubscriptionForm.jsx
+++ src/components/SubscriptionForm/SubscriptionForm.jsx
@@ -1,14 +1,16 @@
 import styles from "./SubscriptionForm.module.scss";
-
+import { useTranslation } from "react-i18next";
 
 const SubscriptionForm = () => {
+    const {t} = useTranslation();
+
     return (
         <section className={styles.subscription}>
-            <h2 className={styles["subscription-title"]}>Підпишись</h2>
-            <p className={styles["subscription-subtitle"]}>І будь в курсі останніх новинок</p>
+            <h2 className={styles["subscription-title"]}>{t("subscription.title")}</h2>
+            <p className={styles["subscription-subtitle"]}>{t("subscription.subtitle")}</p>
             <form action="POST" className={styles["subscription-form"]}>
-                <input type="email" name="email" className={styles["subscription-form-email"]} placeholder="Введіть адресу електронної пошти" autoComplete="email" />
-                <button type="submit" className={styles["subscription-form-btn"]}>Підписатись</button>
+                <input type="email" name="email" className={styles["subscription-form-email"]} placeholder={t("subscription.form.placeholder")} />
+                <button type="submit" className={styles["subscription-form-btn"]}>{t("subscription.form.button")}</button>
             </form>
         </section>
     )
